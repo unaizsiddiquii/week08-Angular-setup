12899ef30a1e503f9fac10d9b49676ed16b9007b
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-06 23:19:02 +0530

    Unaiz - Display user inputted name and change design

--- HELLO-WORLD/src/app/app.component.ts
+++ HELLO-WORLD/src/app/app.component.ts
@@ -1,22 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
 })
 export class AppComponent {
   title = 'HELLO-WORLD';
   imgUrl =
     'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
   url = 'https://www.bridgelabz.com/';
+
+  userName: string = '';
   onClick() {
     window.open(this.url, '_blank');
   }
 
   ngOnInit(): void {
-    this.title = 'Hello from BridgeLabz';
+    this.title = ' from BridgeLabz';
   }
 }
