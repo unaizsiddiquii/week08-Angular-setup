3f112d4e1f9c8215c90c0c719a4a4a287b9119bc
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-06 15:35:22 +0530

    Unaiz -  Add. click event on image (on imge click it goes to bridgelabz site)

--- HELLO-WORLD/src/app/app.component.ts
+++ HELLO-WORLD/src/app/app.component.ts
@@ -11,6 +11,11 @@ export class AppComponent {
   title = 'HELLO-WORLD';
   imgUrl =
     'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
+  url = 'https://www.bridgelabz.com/';
+  onClick() {
+    window.open(this.url, '_blank');
+  }
+
   ngOnInit(): void {
     this.title = 'Hello from BridgeLabz';
   }
