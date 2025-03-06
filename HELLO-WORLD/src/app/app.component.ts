b3ce36e8d62dc4e38f47abf57f26289f9ae5fe28
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-06 15:18:56 +0530

    Unaiz - Add image display and style it

--- HELLO-WORLD/src/app/app.component.ts
+++ HELLO-WORLD/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HELLO-WORLD';
+  imgUrl =
+    'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
   ngOnInit(): void {
     this.title = 'Hello from BridgeLabz';
   }
