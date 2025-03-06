208fb507c760cc0d1942ffac38f1f9ea59b114cd
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-06 14:02:47 +0530

    Unaiz Siddiqui - Angular setup and Displaying Greet message

---
+++ HELLO-WORLD/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'HELLO-WORLD';
+  ngOnInit(): void {
+    this.title = 'Hello from BridgeLabz';
+  }
+}
